d03f68f1d604e06eacfbc098e8fc0c6c4c59488d
Exodus <zjemcikolege@protonmail.com>
2017-10-09 02:30:41 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,7 @@ TARGET_OTA_ASSERT_DEVICE := g3,d855
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d855/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/lge/d855/bluetooth/vnd_g3.txt
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_d855_defconfig
